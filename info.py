7998edc890b6ea4f2aefb03a600eb4b9e2998cc2
Autofiltertg <t.metghj123@gmail.com>
2024-09-09 18:51:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -123,7 +123,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "purring-marsha/auto-filter-bot") #if heroku then paste the app link here ex: https://heroku......./
+URL = environ.get("URL", "https://purring-marsha/auto-filter-bot") #if heroku then paste the app link here ex: https://heroku......./
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
